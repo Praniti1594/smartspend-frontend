8aed00ed20c792bf545c9151c09a9dcf20b18740
Praniti  Kubal <140830559+Praniti1594@users.noreply.github.com>
2025-07-02 17:12:45 +0530

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./profile.css";
+// import "./profile.css";
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
